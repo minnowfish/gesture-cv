a66856482ea07efb3cf6076e78fe04cf3de24a76
minnowfish <ashlynchor15@gmail.com>
2026-08-30 22:37:42 +0800

    feat: export model to torchscript

--- training/export.py
+++ training/export.py
@@ -1,7 +1,22 @@
-import torch 
+import torch
+
+from config import MODEL_SCRIPT_PATH, MODEL_WEIGHTS_PATH
+from model import Model
+
 
 def main():
-    model = torch.load("model/model.pth")
+    model = Model()
+    try:
+        model.load_state_dict(torch.load(MODEL_WEIGHTS_PATH))
+    except (FileNotFoundError):
+        print("Run train.py first")
+        return
+    model.eval()
+    script_model = torch.jit.script(model)
+    script_model.save(MODEL_SCRIPT_PATH)
+
+    print(f"Exported to {MODEL_SCRIPT_PATH}")
+
 
 if __name__ == "__main__":
     main()
